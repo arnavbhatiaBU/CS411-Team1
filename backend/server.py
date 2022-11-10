b0976705823b71c49c4b0d123c178e00d3cda58c
Arnav Bhatia <arnavbhatia@crc-dot1x-nat-10-239-237-59.bu.edu>
2022-11-09 19:12:27 -0500

    national holiday working

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,25 @@
 from flask import Flask
+import requests
+import json
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def testing_route():
     return "this is working"
+
+@app.route("/nationalholiday")
+def hello():
+    api_key = ""
+    with open("./config.json") as f:
+        config = json.load(f)
+        api_key = config["nationalHolidayKey"]
+    
+    url = f"https://holidayapi.com/v1/holidays?pretty&key={api_key}&country=US&year=2021"
+
+    response = requests.request("GET", url)
+
+    print(type(response.json()))
+
+    return response.json()
