fcf1e96a78753f91410351b9471e316ec04390a5
Arnav Bhatia <arnavbhatia@crc-dot1x-nat-10-239-237-59.bu.edu>
2022-11-09 20:06:56 -0500

    finish data display

--- frontend/classproject/src/App.js
+++ frontend/classproject/src/App.js
@@ -1,12 +1,15 @@
 import NavBar from './components/NavBar';
 import { useState } from "react";
+import DataDisplay from "./components/DataDisplay";
 
 function App() {
   const [searchRes, setSearchRes] = useState("");
   return (
     <div className="text-center">
       <NavBar updateRes={setSearchRes}></NavBar>
-      {console.log(searchRes)}
+      <ul>
+        {searchRes.map(each => <DataDisplay name={each.name}></DataDisplay>)}
+      </ul>
     </div>
   );
 }
---
+++ frontend/classproject/src/components/DataDisplay.jsx
@@ -0,0 +1,7 @@
+const DataDisplay = (props) => {
+    return(
+        <li className="my-4 border-2 border-gray-500 rounded-md p-2 w-1/2 mx-auto">{props.name}</li>
+    )
+}
+
+export default DataDisplay;
