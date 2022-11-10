02bea74e6d5fbdfab18b749d142fe113ea943295
Arnav Bhatia <arnavbhatia@crc-dot1x-nat-10-239-237-59.bu.edu>
2022-11-09 19:38:27 -0500

    backend fully working

--- backend/server.py
+++ backend/server.py
@@ -28,8 +28,22 @@ def holiday_by_date():
     lookup_day = request.args.get("day")
     lookup_month = request.args.get("month")
     lookup_year = request.args.get("year")
-    return {
-        "year": lookup_year,
-        "month": lookup_month,
-        "day": lookup_day
-    }
+
+    api_key = ""
+    with open("./config.json") as f:
+        config = json.load(f)
+        api_key = config["nationalHolidayKey"]
+    
+    url = f"https://holidayapi.com/v1/holidays?pretty&key={api_key}&country=US&year=2021"
+
+    raw_response = requests.request("GET", url)
+    parsed_response = raw_response.json()
+
+    res = []
+
+    for each_holiday in parsed_response["holidays"]:
+        if each_holiday["date"] == f"{lookup_year}-{lookup_month}-{lookup_day}":
+            res.append(each_holiday)
+
+    return res
+    
