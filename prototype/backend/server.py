82f1aeebf369a89b2b260c1eccb300e3c8640ab3
Arnav Bhatia <arnavbhatia@Arnavs-MacBook-Air.local>
2022-12-04 18:43:02 -0500

    finish connecting to spotify api

--- prototype/backend/server.py
+++ prototype/backend/server.py
@@ -48,3 +48,47 @@ def holiday_by_date():
             res.append(each_holiday)
 
     return res
+
+def get_track(holiday_name):
+    # Get the access token
+    auth_url = "https://accounts.spotify.com/api/token"
+
+    clientID = ""
+    clientSecret = ""
+
+    with open("./config.json") as f:
+        config = json.load(f)
+        clientID = config["spotifyClientID"]
+        clientSecret = config["clientSecret"]
+
+    data = {
+        'grant_type': 'client_credentials',
+        'client_id': clientID,
+        'client_secret': clientSecret,
+    }
+
+    auth_res = requests.post(url=auth_url, data=data)
+    access_token = auth_res.json().get('access_token')
+
+    header = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {access_token}"    
+    }
+
+    res = requests.get(url="https://api.spotify.com/v1/search?q=christmas&type=track", headers=header)
+
+    res_cleaned = res.json()
+    res_tracks = res_cleaned["tracks"]
+
+    track_res = []
+
+    for each_track in res_tracks["items"]:
+        temp = {
+            "name": each_track["name"],
+            "artist": [e["name"] for e in each_track["artists"]],
+            "album": each_track["album"]["name"]
+        }
+        track_res.append(temp)
+    
+    return track_res
